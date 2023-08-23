df1ec858eb1ed79d2649ec34b6b216748d5b6859
Navina K <99734360+navina1@users.noreply.github.com>
2023-08-23 15:38:45 +0530

    guessing game completed

--- Week-3/guessingGame.js
+++ Week-3/guessingGame.js
@@ -1,27 +1,27 @@
 function playGuessingGame(numToGuess, totalGuesses = 10) {
-  var count = 1;
-  var message="Enter a number between 1 and 100."
-  while (count <= totalGuesses) {
-    let userInput = prompt(message);
-    //console.log(userInput);
-    if(userInput==null){
-        return 0;
-    }
-    if(isNaN(userInput) || userInput==""){
-        message = `Please enter a number.(${totalGuesses - count} attempt left)`;
-        continue;
-    }  
-    if (userInput == numToGuess) {
-        alert(`You guessed the number ${numToGuess} in ${count} attempt`);
-        return (count);
-    }
-    else if (userInput < numToGuess) {
-        message=`${userInput}is too small. Guess a larger number.(${totalGuesses-count} attempt left)`
-    } else {
-        message=`${userInput}is too large. Guess a small number.(${totalGuesses-count} attempt left)`
-    }
-    
-    count++;
+    var count = 0;
+    var message="Enter a number between 1 and 100."
+    while (count < totalGuesses) {
+        count++;
+        let userInput = prompt(message);
+        //console.log(userInput);
+        if(userInput==null){
+            return 0;
+        }
+        if(isNaN(userInput) || userInput==""){
+            count = count - 1;
+            message = `Please enter a number.(${totalGuesses - count} attempt left)`;
+            continue;
+        }  
+        if (userInput == numToGuess) {
+            alert(`You guessed the number ${numToGuess} in ${count} attempt`);
+            return (count);
+        }
+        else if (userInput < numToGuess) {
+            message=`${userInput}is too small. Guess a larger number.(${totalGuesses-count} attempt left)`
+        } else {
+            message=`${userInput}is too large. Guess a small number.(${totalGuesses-count} attempt left)`
+        }
     }
     alert(`Game Over, the number is ${numToGuess}`);
     return 0;
