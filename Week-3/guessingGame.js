9127e473ac247f52efacc6c22a1894400501fbe5
Navina K <navinak321@gmail.com>
2023-08-27 22:55:27 +0530

    input range updated for guessing game

--- Week-3/guessingGame.js
+++ Week-3/guessingGame.js
@@ -1,26 +1,31 @@
 function playGuessingGame(numToGuess, totalGuesses = 10) {
-    var count = 0;
-    var message="Enter a number between 1 and 100."
+    let count = 0;
+    let message = "Enter a number between 1 and 100."
     while (count < totalGuesses) {
         count++;
         let userInput = prompt(message);
         //console.log(userInput);
-        if(userInput==null){
+        if (userInput == null) {
             return 0;
         }
-        if(isNaN(userInput) || userInput==""){
+        if (isNaN(userInput) || userInput == "") {
             count = count - 1;
             message = `Please enter a number.(${totalGuesses - count} attempt left)`;
             continue;
-        }  
+        }
+        if (userInput < 1 || userInput > 100) {
+            count = count - 1;
+            message = `Please enter a number between 1 and 100.(${totalGuesses - count} attempt left)`;
+            continue;
+        }
         if (userInput == numToGuess) {
             alert(`You guessed the number ${numToGuess} in ${count} attempt`);
             return (count);
         }
         else if (userInput < numToGuess) {
-            message=`${userInput}is too small. Guess a larger number.(${totalGuesses-count} attempt left)`
+            message = `${userInput}is too small. Guess a larger number.(${totalGuesses - count} attempt left)`
         } else {
-            message=`${userInput}is too large. Guess a small number.(${totalGuesses-count} attempt left)`
+            message = `${userInput}is too large. Guess a small number.(${totalGuesses - count} attempt left)`
         }
     }
     alert(`Game Over, the number is ${numToGuess}`);
