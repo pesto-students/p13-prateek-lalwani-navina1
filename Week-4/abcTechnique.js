916b918c2f83998da3d25a4e762eb92ffbb1ee3f
Navina K <99734360+navina1@users.noreply.github.com>
2023-08-28 15:35:31 +0530

    call bind apply method completed

--- Week-4/abcTechnique.js
+++ Week-4/abcTechnique.js
@@ -1,39 +1,52 @@
 class Calculator {
-    constructor(num1, num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+  add(num1, num2) {
+    return ` Sum is ${num1 + num2}`;
+  }
+  subtract(num1, num2) {
+    return ` Difference is ${num1 - num2}`;
+  }
+  multiply(num1, num2) {
+    return ` Product is ${num1 * num2}`;
+  }
+  divide(num1, num2) {
+    if (num2 === 0) {
+      return "Cannot divide by zero";
     }
-    add() {
-        return (` Sum is ${this.num1 + this.num2}`);
-    }
-    subtract() {
-        return (` Difference is ${this.num1 - this.num2}`);
-    }
-    multiply() {
-        return (` Product is ${this.num1 * this.num2}`);
-    }
-    divide() {
-        if (this.num2 === 0) {
-            return ("Cannot divide by zero");
-        }
-        return (` Quotient is ${this.num1 / this.num2}`);
-    }
+    return ` Quotient is ${num1 / num2}`;
+  }
 }
 class ScientificCalculator extends Calculator {
-    constructor(number) {
-        this.number = number
-    }
-    square() {
-        return this.number * this.number;
-    }
-    cube() {
-        return this.number * this.number * this.number;
-    }
-    power(base, exponent) {
-        return Math.pow(base, exponent);
-    }
+  square(num) {
+    return this.number * this.number;
+  }
+  cube(num) {
+    return this.number * this.number * this.number;
+  }
+  power(num, power) {
+    return num ** power;
+  }
 }
-const ScientificCalculator1=new ScientificCalculator(5);
+const scientificCalculator1 = new ScientificCalculator();
 // Using "call" method to invoke "add" method of Calculator class
-const resultAdd=Calculator.prototype.add.call(ScientificCalculator1,10,5);
+const resultAdd = scientificCalculator1.add.call(scientificCalculator1, 10, 5);
 console.log("Using call - Addition:", resultAdd);
+
+const resultSub = scientificCalculator1.subtract.apply(
+  scientificCalculator1,
+  [10, 5]
+);
+console.log("Using apply - substraction:", resultSub);
+
+function multiplyByTwo(num) {
+  return num * 2;
+}
+
+const resultMultiplyByTwo = multiplyByTwo.bind(scientificCalculator1, 5);
+console.log(`multiply by two using bind ${resultMultiplyByTwo()}`);
+
+function powerOfThree(num) {
+  return num * num * num;
+}
+
+const resultPowerOfThree = powerOfThree.bind(scientificCalculator1, 2);
+console.log(`Cube of two is ${resultPowerOfThree()}`);
