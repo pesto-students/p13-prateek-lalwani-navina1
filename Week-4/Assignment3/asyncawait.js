0847c1f48aa8464e332e91e48db455cec9d092f3
Navina K <99734360+navina1@users.noreply.github.com>
2023-08-29 22:25:24 +0530

    asyncawait started

---
+++ Week-4/Assignment3/asyncawait.js
@@ -0,0 +1,20 @@
+async function getExchangeRate(currencyCode) {
+    try {
+        const response = await fetch('https://api.exchangerate.host/latest');
+        const data = await response.json();
+        const exchangeRate = data.rates[currencyCode];
+        if (exchangeRate === undefined) {
+            throw new Error(`Exchange rate not available for currency code: ${currencyCode}`);
+        }
+        const roundedExchangeRate = parseFloat(exchangeRate).toFixed(4);
+        
+        return roundedExchangeRate;
+    } catch (error) {
+        throw new Error(`An error occurred: ${error.message}`);
+    }
+}
+
+const currencyCode = "USD";
+getExchangeRate(currencyCode)
+    .then(rate => console.log(`Exchange rate for ${currencyCode}: ${rate}`))
+    .catch(error => console.error(error.message));
