894705f8810433942d7b67ab437cca21c173e8bb
Navina K <navinak321@gmail.com>
2023-09-05 14:18:12 +0530

    Assignment 2.2 completed

---
+++ Week-5/Assignment2.2/script.js
@@ -0,0 +1,10 @@
+function displayWindowProperties() {
+    console.log(window.navigator.userAgent);
+    console.log(window.screen.width);
+    console.log(window.screen.height);
+    console.log(window.location.href);
+    console.log(window.location.pathname);
+    console.log(window.localStorage)
+    console.log(window.sessionStorage)
+}
+displayWindowProperties()
