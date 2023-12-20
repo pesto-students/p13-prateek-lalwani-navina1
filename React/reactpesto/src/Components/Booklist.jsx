d9d9946a6e4b9fb4716d204a1c7f14ad26365b0a
Navina K <navinak321@gmail.com>
2023-12-20 15:46:34 +0530

    navbar completed

--- React/reactpesto/src/Components/Booklist.jsx
+++ React/reactpesto/src/Components/Booklist.jsx
@@ -13,70 +13,72 @@ function Booklist() {
     //     // Add more books if you'd like
     //   ];
 
-     const [searchWord,setSearchWord]=useState('');
-     const [books,setBooks]=useState([]); 
-     const addBook=(book)=>{
+    const [searchWord, setSearchWord] = useState('');
+    const [books, setBooks] = useState([]);
+    const addBook = (book) => {
         setBooks([...books, book]);
-     }
-     const deleteBook = (index) => {
+    }
+    const deleteBook = (index) => {
         const updatedBooks = [...books];
         updatedBooks.splice(index, 1);
         setBooks(updatedBooks);
-      };
-      let filteredBooks=useBookFilter(books,searchWord);
-      console.log(filteredBooks)
-      //pagination
-    const itemsPerPage=4;
-    const [currentPage,setCurrentPage]=useState(1);
-    const startIndex=(currentPage-1)* itemsPerPage;
-    const endIndex=startIndex+itemsPerPage;
+    };
+    let filteredBooks = useBookFilter(books, searchWord);
+    console.log(filteredBooks)
+    //pagination
+    const itemsPerPage = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
 
-    const paginateBooks=filteredBooks.slice(startIndex,endIndex);
-    const totalPage=Math.ceil(filteredBooks.length/itemsPerPage);
-    const handlePage=(newPage)=>{
+    const paginateBooks = filteredBooks.slice(startIndex, endIndex);
+    const totalPage = Math.ceil(filteredBooks.length / itemsPerPage);
+    const handlePage = (newPage) => {
         setCurrentPage(newPage)
     }
-    useEffect(()=>{
+    useEffect(() => {
 
-    },[books])
-  return (
-    <div className="booklist-container">
-        <div className='navbar'>
-            <h1>Booklist</h1>
-            <input 
-                type='text' 
-                placeholder='Search by title' 
-                value={searchWord} 
-                onChange={(e)=>setSearchWord(e.target.value)
-            }/>
-            <button>Switch to dark theme</button>
-        </div>
-        <BookForm addBook={addBook}/>
-        {
-            books?.length<1 ?
-            <p>No Books Yet!</p>:
-            <ul className="book-list">
-            {paginateBooks.map((book,index)=>(
-                // <li key={index}>{book.title} by {book.author}, {book.year}</li>
-                <Book key={index} title={book.title} author={book.author} year={book.year} deleteBook={()=>deleteBook(index)}/>
-            ))}
-        </ul>
-
-        }
-        {books.length>0 && <div className='pagination'>
-            <p>Page {currentPage} of {totalPage}</p>
-            <div className='page'>
-                <button onClick={() => handlePage(currentPage - 1)} disabled={currentPage === 1}>
-                    Previous
-                </button>
-                <span style={{ margin: '0 10px' }}></span> 
-                <button onClick={() => handlePage(currentPage + 1)} disabled={currentPage === totalPage}>
-                    Next
-                </button>
+    }, [books])
+    return (
+        <>
+            <div className='navbar'>
+                <h1>Booklist</h1>
+                <input
+                    type='text'
+                    placeholder='Search by title'
+                    value={searchWord}
+                    onChange={(e) => setSearchWord(e.target.value)
+                    } />
+                <button>Switch theme</button>
             </div>
-        </div>}
-    </div>
-  )
+            <div className="booklist-container">
+                <BookForm addBook={addBook} />
+                {
+                    books?.length < 1 ?
+                        <p>No Books Yet!</p> :
+                        <ul className="book-list">
+                            {paginateBooks.map((book, index) => (
+                                // <li key={index}>{book.title} by {book.author}, {book.year}</li>
+                                <Book key={index} title={book.title} author={book.author} year={book.year} deleteBook={() => deleteBook(index)} />
+                            ))}
+                        </ul>
+
+                }
+                {books.length > 0 && <div className='pagination'>
+                    <p>Page {currentPage} of {totalPage}</p>
+                    <div className='page'>
+                        <button onClick={() => handlePage(currentPage - 1)} disabled={currentPage === 1}>
+                            Previous
+                        </button>
+                        <span style={{ margin: '0 10px' }}></span>
+                        <button onClick={() => handlePage(currentPage + 1)} disabled={currentPage === totalPage}>
+                            Next
+                        </button>
+                    </div>
+                </div>}
+            </div>
+        </>
+    )
 }
 
 export default Booklist
